c24ee9f495093eda2f71f7f279358653f73c3d9e
rchrdkvcs <gumimacihun@gmail.com>
2026-06-08 10:40:51 +0200

    feat(app): préparer Flask pour l'exécution en conteneur

--- __init__.py
+++ __init__.py
@@ -1,17 +1,19 @@
+import os
+
 from flask import Flask
 from flask import render_template
-from flask import json
-import sqlite3
-                                                                                                                                       
-app = Flask(__name__)                                                                                                                  
-                                                                                                                                       
-@app.route('/')
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
 def hello_world():
     return "<h2>Bonjour tout le monde !</h2><p>Pour accéder à vos exerices cliquez <a href='./exercices/'>Ici</a></p>"
 
-@app.route('/exercices/')
+@app.route('/exercices/', methods=['GET'])
 def exercices():
     return render_template('exercices.html')
-                                                                                                               
+
 if __name__ == "__main__":
-  app.run(debug=True)
+    port = int(os.environ.get("PORT", "5000"))
+    debug = os.environ.get("FLASK_DEBUG", "0") == "1"
+    app.run(host="0.0.0.0", port=port, debug=debug)
